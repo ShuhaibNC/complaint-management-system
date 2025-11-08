6acf60df3d52921e67b52e63ba673f8b63d41574
ShuhaibNC <shuhaibsaad5@gmail.com>
2025-11-09 01:00:40 +0530

    Sign up set

--- complaint_management_system/settings.py
+++ complaint_management_system/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
+    'user',
+    'complaint_manager',
+    'sho',
+    'system_manager',
 ]
 
 MIDDLEWARE = [
--- user/views.py
+++ user/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+def signup(request):
+    return render(request, 'register/signup.html')
+
+def register_complaint(request):
+    return render(request, 'register/complaint_register.html')
