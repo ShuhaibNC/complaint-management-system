a4619a85fda58ed6c94aaee6ff17d5a325e88ba1
ShuhaibNC <shuhaibsaad5@gmail.com>
2026-05-06 17:19:36 +0530

    fix

---
+++ README.md
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '', # Enter Database name here
+        'USER': '', # Enter Database name here
+        'PASSWORD': '', # Enter DB Password here
+        'HOST': 'localhost', # Enter Host here
+        'PORT': '3306', # Enter Port here
+    }
+}
--- complaint_management_system/settings.py
+++ complaint_management_system/settings.py
@@ -85,11 +85,11 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'cms',
-        'USER': 'root',
-        'PASSWORD': '123', 
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'NAME': '', # Enter Database name here
+        'USER': '', # Enter Database name here
+        'PASSWORD': '', # Enter DB Password here
+        'HOST': 'localhost', # Enter Host here
+        'PORT': '3306', # Enter Port here
     }
 }
 
